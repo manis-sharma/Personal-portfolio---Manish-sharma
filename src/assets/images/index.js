ecc932d89baf9cc940b65bca982665313fce1fe7
manis-sharma <sharma.250th@gmail.com>
2025-01-24 21:52:10 +0545

    NEW COMMIT

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+import hero from "./hero.jpg";
+import logo from "./logo.svg";
+import nereus from "./nereus.png";
+import artemis from "./artemis.jpg";
+import natours from "./natours.png";
+import gpt_3 from "./gpt_3.png";
+import wild_oasis from "./wild_oasis.png";
+import promotion from "./promotion.png";
+
+export { hero, logo, nereus, artemis, natours, gpt_3, wild_oasis, promotion };
---
+++ src/constants/index.js
@@ -0,0 +1,211 @@
+import {
+    nereus,
+    artemis,
+    wild_oasis,
+    natours,
+    gpt_3,
+    promotion,
+  } from "../assets/images";
+  import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    messenger,
+    spotify,
+    duolingo,
+  } from "../assets/icons";
+  
+  export const skills = [
+    {
+      imageUrl: html,
+      name: "HTML",
+      type: "Frontend",
+    },
+    {
+      imageUrl: css,
+      name: "CSS",
+      type: "Frontend",
+    },
+    {
+      imageUrl: javascript,
+      name: "JavaScript",
+      type: "Frontend",
+    },
+    {
+      imageUrl: tailwindcss,
+      name: "Tailwind CSS",
+      type: "Frontend",
+    },
+    {
+      imageUrl: react,
+      name: "React",
+      type: "Frontend",
+    },
+    {
+      imageUrl: redux,
+      name: "Redux",
+      type: "State Management",
+    },
+    {
+      imageUrl: typescript,
+      name: "TypeScript",
+      type: "Frontend",
+    },
+    {
+      imageUrl: nextjs,
+      name: "Next.js",
+      type: "Frontend",
+    },
+  
+    {
+      imageUrl: nodejs,
+      name: "Node.js",
+      type: "Backend",
+    },
+    {
+      imageUrl: mongodb,
+      name: "MongoDB",
+      type: "Database",
+    },
+    {
+      imageUrl: express,
+      name: "Express",
+      type: "Backend",
+    },
+    {
+      imageUrl: git,
+      name: "Git",
+      type: "Version Control",
+    },
+    {
+      imageUrl: github,
+      name: "GitHub",
+      type: "Version Control",
+    },
+  ];
+  
+  export const experiences = [
+    {
+      title: "Merchant Navy Officer",
+      company_name: "Nereus Shipping S.A.",
+      icon: nereus,
+      iconBg: "#accbe1",
+      date: "April 2015 - October 2015 & June 2017 - February 2018",
+      points: [
+        "During the two training voyages as part of my studies at the Merchant Marine Academy of Aspropyrgos, I came across various responsibilities.",
+        "Assistant in the safe navigation of the ship, maintenance and update of navigation and safety equipment, as well as the operation of loading and unloading while taking measures.",
+        "Participation in manual tasks such as mooring and unmooring the ship, painting and maintaining the deck, and inventorying supplies.",
+      ],
+    },
+    {
+      title: "Fiber Optic Technician",
+      company_name: "Artemis ITS",
+      icon: artemis,
+      iconBg: "#A1A5A6",
+      date: "October 2021 - March 2023",
+      points: [
+        "I was living in Germany at that period, and through that job, I gained several valuable experiences such as collaboration and goal-setting.",
+        "Introduction and installation of fiber optic cables and devices in homes.",
+        "Soldering of distribution panels at nodes before homes as well as at central points.",
+        "Measurement and quality control of the network.",
+      ],
+    },
+  ];
+  
+  export const socialLinks = [
+    {
+      name: "Contact",
+      iconUrl: contact,
+      link: "/contact",
+    },
+    {
+      name: "GitHub",
+      iconUrl: github,
+      link: "https://github.com/dimitrispalamidas",
+    },
+    {
+      name: "LinkedIn",
+      iconUrl: linkedin,
+      link: "https://www.linkedin.com/in/dimitris-palamidas",
+    },
+  ];
+  
+  export const projects = [
+    {
+      iconUrl: duolingo,
+      theme: "btn-back-red",
+      name: "Duolingo-clone",
+      description:
+        "Developed this well-known application that help users learning easily new languages.",
+      link: "https://duolingo-clone-blue.vercel.app/",
+    },
+    {
+      iconUrl: spotify,
+      theme: "btn-back-green",
+      name: "Spotify-clone",
+      description:
+        "Created a full-stack Spotify clone, a music application on which you can import and play your favourite songs.",
+      link: "https://spotify-clone-delta-five.vercel.app/",
+    },
+    {
+      iconUrl: messenger,
+      theme: "btn-back-blue",
+      name: "Messenger-clone",
+      description:
+        "Designed and built a responsive messenger chat application, allowing users to share photos, create group chats and connect with friends in a familiar social media environment",
+      link: "https://messenger-clone-tau-three.vercel.app/",
+    },
+    {
+      iconUrl: wild_oasis,
+      theme: "btn-back-green",
+      name: "The Wild Oasis",
+      description:
+        "The Wild Oasis hotel management app is a full-featured React web application that allows hotel employees to manage cabins, bookings, and guests.",
+      link: "https://the-wild-oasis-flame.vercel.app/",
+    },
+    {
+      iconUrl: natours,
+      theme: "btn-back-black",
+      name: "Natours Project",
+      description:
+        "This web application allows it's users to book tour vacations.A tour refers to a series of locations, specially picked to excite the adventurous spirit of the individual who books it.",
+      link: "https://github.com/dimitrispalamidas/natours-project",
+    },
+    {
+      iconUrl: gpt_3,
+      theme: "btn-back-pink",
+      name: "GPT-3",
+      description:
+        "A fully-responsive website with modern UI/UX design, offering an impressive and user-friendly experience.",
+      link: "https://gpt-3-eosin-two.vercel.app/",
+    },
+    {
+      iconUrl: promotion,
+      theme: "btn-back-red",
+      name: "S.T.Promotion (Real Freelancing Project)",
+      description:
+        "A sleek and modern website designed for a dynamic promotion company that brings brands to life.",
+      link: "https://promotion-website.vercel.app/",
+    },
+  ];
